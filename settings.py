49208d31a47ed67ad0a2a5e33ac837399f8442a6
sunlei-xxp <sun-lei@163.com>
2021-02-16 20:07:26 +0800

    更新settings

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ class Settings():
 
         # Setting for bullet
         self.bullet_speed_factor = 1
-        self.bullet_width = 600
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (255, 0, 0)
         self.bullets_allowed = 3
