b0bad8aff847f645b95c37acdb16ad76519ff5fb
sunlei-xxp <sun-lei@163.com>
2021-03-13 00:19:59 +0800

    修复一个子弹同时击中多个外星人的问题

--- game_functions.py
+++ game_functions.py
@@ -117,8 +117,9 @@ def check_bullets_aliens_collisions(ai_settings, screen, stats, sb, ship, aliens
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if collisions:
-        stats.score += ai_settings.alien_points
-        sb.prep_score
+        for aliens in collisions.values():
+            stats.score += ai_settings.alien_points*len(aliens)
+            sb.prep_score()
 
     if len(aliens) == 0:
         # Remove bullets, 加快游戏节奏, and create a new group of alien
